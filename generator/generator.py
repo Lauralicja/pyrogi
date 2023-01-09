db68dcd3cd02ba2f2f281cf38562fd961de0ca3b
Laura Żuchowska 🐛 <zuchowska.laura@gmail.com>
2023-01-09 18:03:18 +0100

    Added geenration methods

--- generator/generator.py
+++ generator/generator.py
@@ -1,8 +1,21 @@
 from models.events import EventModel
 from models.user import UserModel
+import json
+    
+def generate_users(amount: int = 1):
+    users = []
+    for _ in range(0, amount):
+        randomized_user = UserModel()
+        users.append(randomized_user.json())
+    return users
+    
+def generate_events(user_id: str, amount: int = 1):
+    events = []
+    for _ in range(0, amount):
+        randomized_event = EventModel(user_id = user_id)
+        events.append(randomized_event.json())
+    return events
 
-event_random = EventModel()
-print(event_random)
-
-user_random = UserModel()
-print(user_random)
+users = generate_users()
+user = json.loads(users[0])
+events = generate_events(user["user_id"], 1)
