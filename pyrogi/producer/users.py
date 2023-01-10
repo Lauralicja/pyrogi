bc42a6703eb1f3971ff26662b0d30535e00fff5e
Laura Żuchowska 🐛 <zuchowska.laura@gmail.com>
2023-01-10 09:31:06 +0100

    Added serializers

--- pyrogi/producer/users.py
+++ pyrogi/producer/users.py
@@ -1,3 +1,8 @@
 from kafka import KafkaProducer
+import json
 
-producer = KafkaProducer(bootstrap_servers='localhost:29092')
+producer = KafkaProducer(bootstrap_servers='localhost:29092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+# add id to redis
+# add to topic
+# save
