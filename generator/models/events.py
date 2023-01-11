d4e49c82595d76ea955829c7341e534d51583168
Laura Żuchowska 🐛 <zuchowska.laura@gmail.com>
2023-01-11 12:45:59 +0100

    Added docstings

--- generator/models/events.py
+++ generator/models/events.py
@@ -9,6 +9,25 @@ from pydantic import BaseModel, Field, validator
 fake_generator = Faker()
 
 class EventModel(BaseModel):
+    """pyndatic model for `Event`
+    
+    Allows for a quick generation of various properties.
+    This model mocks the creation of user action (event) on a page.
+    The event is rating a movie/book/song...
+    The mocking itself is created with the use of Faker library.
+    
+    Optional parameters have a certain probability to be set as well,
+    providing the variety of customers, as some of them fill the rating only,
+    and some of them write comments.
+    
+    Mandatory fields:
+    - user_id - an id of a user, set by reading the user's id
+    - timestamp - automatically created by page
+    - rating - provided by a customer
+
+    Args:
+        BaseModel: inheritance from pydantic BaseModel
+    """
     user_id: str = Field(default_factory = uuid4)
     timestamp: str = None
     rating: int = None
