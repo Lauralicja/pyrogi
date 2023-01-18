dd4f85e59212757ef70f514ba795e0eed2364f54
Laura Żuchowska 🐛 <zuchowska.laura@gmail.com>
2023-01-18 13:50:33 +0100

    Added Users Kafka producer

--- pyrogi/producer/users.py
+++ pyrogi/producer/users.py
@@ -1,16 +1,26 @@
-from kafka import KafkaProducer
 import json
+import time
+
 import requests
+from kafka import KafkaProducer
 
 PYROG_SIZE = 161879
+TOPIC_NAME = "producer-users"
 
-
-producer = KafkaProducer(bootstrap_servers='localhost:29092', 
-                         client_id = "kafka-producer-users", 
+producer = KafkaProducer(bootstrap_servers='broker:29092', 
+                         value_serializer=lambda user: json.dumps(user).encode('utf-8'),
+                         api_version=(0,11,5),
                          retries = 3,
                          batch_size = PYROG_SIZE)
 
 
-
-# polling on events_proxy GET endpoint
-# if found send to topic
+def run(): 
+    offset = 0
+    while(True):
+        response = requests.get("http://proxys:5000/users", timeout=3)
+        if response.text:
+            result = producer.send(TOPIC_NAME, response.text)
+            logging.debug(result)
+            producer.flush()
+        time.sleep(2)
+run()
